cdc4c93728808e1f1423107cc242a1634775c294
FussyMarmot <dimafofanov87@gmail.com>
2026-02-22 22:47:45 +0300

    added support .env in settings

--- dnd_companion/settings.py
+++ dnd_companion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)r%u-m9nz0_=r*k8!&m3ak*-tq&bw*y2%a_)qboaus)uub*5%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
