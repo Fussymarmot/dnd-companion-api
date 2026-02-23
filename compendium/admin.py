676593dc2dd1949c3d9bc6c0c858e5f6b09090ff
FussyMarmot <dimafofanov87@gmail.com>
2026-02-23 18:49:42 +0300

    Added models to the admin panel

--- compendium/admin.py
+++ compendium/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Spells, CharactersClass, Races, Monsters
 # Register your models here.
+admin.site.register(Spells)
+admin.site.register(CharactersClass)
+admin.site.register(Races)
+admin.site.register(Monsters)
